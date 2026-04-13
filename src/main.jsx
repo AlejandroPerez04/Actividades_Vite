fcfac6604f853d6cb83febb2cc82ae96153a1935
Alejandro Perez <alejandroperez11014@gmail.com>
2026-04-13 09:32:48 -0700

    Implementacion de paginas de informacion acerca de mi 2.o

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
-
-export default Root;
