10d4ba25850f2c60fbe50b14e58a188bd575d2c3
Alejandro Perez <alejandroperez11014@gmail.com>
2026-04-13 10:25:28 -0700

    Implementacion de paginas de informacion acerca de mi 3.2

--- src/pages/Tablas.jsx
+++ src/pages/Tablas.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.svg'
 import heroImg from '../assets/hero.png'
-import './App.css'
+import '../App.css'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
